661a3de381b39d7742541edce66f95904d9993c3
anusmaqsood001 <aunsmaqsood@gmail.com>
2026-07-20 20:43:14 +0500

    updates streamlit.py

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -2,36 +2,47 @@ import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
 
-# st.session_state -> dict -> 
-CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+CONFIG = {"configurable": {"thread_id": "thread-1"}}
 
-if 'message_history' not in st.session_state:
-    st.session_state['message_history'] = []
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
 
-# loading the conversation history
-for message in st.session_state['message_history']:
-    with st.chat_message(message['role']):
-        st.text(message['content'])
+# Show old messages
+for message in st.session_state["message_history"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-user_input = st.chat_input('Type here')
+user_input = st.chat_input("Type here")
 
 if user_input:
 
-    # first add the message to message_history
-    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
-    with st.chat_message('user'):
-        st.text(user_input)
+    st.session_state["message_history"].append({"role": "user", "content": user_input})
 
-    response = chatbot.invoke({"messages": [HumanMessage(content=user_input)]}, config=CONFIG)
+    with st.chat_message("user"):
+        st.markdown(user_input)
 
-    content = response["messages"][-1].content
+    # Generator function
+    def stream_response():
+        for message_chunk, _ in chatbot.stream(
+            {"messages": [HumanMessage(content=user_input)]},
+            config=CONFIG,
+            stream_mode="messages",
+        ):
+            content = message_chunk.content
 
-    if isinstance(content, list):
-        ai_message = content[0]["text"]
-    else:
-        ai_message = content
+            if isinstance(content, list):
+                for block in content:
+                    if (
+                        isinstance(block, dict)
+                        and block.get("type") == "text"
+                        and block.get("text")
+                    ):
+                        yield block["text"]
+            else:
+                yield str(content)
 
-    # first add the message to message_history
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
-    with st.chat_message('assistant'):
-        st.text(ai_message)
+    # Function 
+    with st.chat_message("assistant"):
+        ai_message = st.write_stream(stream_response)
+
+    st.session_state["message_history"].append({"role": "assistant", "content": ai_message})
